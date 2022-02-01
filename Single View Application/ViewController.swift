26ca6acbc413fd480e4d3019f331b15d03237d12
Motorin Aleksey <aleksey.motorin2015@yandex.ru>
2022-02-01 21:36:12 +0300

    Initial Commit

---
+++ Single View Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Single View Application
+//
+//  Created by Алексей Моторин on 01.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
