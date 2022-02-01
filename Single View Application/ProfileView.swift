727b4962cee01ae2190cb4cfa18789179cb9f056
Motorin Aleksey <aleksey.motorin2015@yandex.ru>
2022-02-01 23:34:13 +0300

    Создал ProfileView.xib, поместил в ProfileView элементы - UIImageView, UILabel 3 шт, UITextView, расположите UI элементы согласно макетам, создал ProfileView.swift, унаследовал от класса UIView. Добавил аутлеты для всех элементов, установил для них имена, загрузил фото для аватара

---
+++ Single View Application/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Single View Application
+//
+//  Created by Алексей Моторин on 01.02.2022.
+//
+
+import Foundation
+import UIKit
+
+class ProfileView: UIView {
+   
+    @IBOutlet var avatarImageView: UIImageView!
+    @IBOutlet var nameLabelView: UILabel!
+    @IBOutlet var birthdayLabelView: UILabel!
+    @IBOutlet var cityLabelView: UILabel!
+    @IBOutlet var aboutDevoloper: UITextView!
+}
